5d55bfbe2e2cb907a431f6774fbffc054c68829d
tim <shuzhzh@gmail.com>
2026-04-16 10:37:47 +0800

    Initial Commit

---
+++ TreeletHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TreeletHub
+//
+//  Created by Tim Shu on 2026/4/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TreeletHub/TreeletHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeletHubApp.swift
+//  TreeletHub
+//
+//  Created by Tim Shu on 2026/4/16.
+//
+
+import SwiftUI
+
+@main
+struct TreeletHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
